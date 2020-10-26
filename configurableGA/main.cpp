4cf02d4da10c31cf14b9c7d295e010a641c1cda4
Furong Ye <f.ye@liacs.leidenuniv.nl>
2020-10-26 23:45:52 +0100

    Initial Commit

---
+++ configurableGA/main.cpp
@@ -0,0 +1,14 @@
+//
+//  main.cpp
+//  configurableGA
+//
+//  Created by Furong Ye on 26/10/2020.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+  // insert code here...
+  std::cout << "Hello, World!\n";
+  return 0;
+}
