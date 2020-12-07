f0fa1f99e188f000221fe85266e79d1471e03e06
FurongYe <furong1993@gmail.com>
2020-12-07 18:37:30 +0100

    A test of EA on w-model extensions of OneMax

--- main.cpp
+++ main.cpp
@@ -16,6 +16,9 @@
 #include "src/sa.h"
 #include "src/sars.h"
 
+
+#include "run/w_model_OneMax_suite.hpp"
+
 using namespace std;
 
 int main(int argc, const char *argv[]) {
@@ -28,23 +31,28 @@ int main(int argc, const char *argv[]) {
   const unsigned seed = 1;
 
   staticEA EA(1, 1); // for comparison with opoea
-  shared_ptr<IOHprofiler_suite<int> > pbo_EA(
-      new PBO_suite(problem_id, instance_id, dimension));
-  EA.run(dir, "EA", pbo_EA, budget, budget, runs, seed);
+  shared_ptr<W_Model_OneMax_suite > EAsuite(
+      new W_Model_OneMax_suite());
+  EA.run(dir, "EA", EAsuite, budget, budget, runs, seed);
+  
+  // staticEA EA(1, 1); // for comparison with opoea
+  // shared_ptr<IOHprofiler_suite<int> > pbo_EA(
+  //     new PBO_suite(problem_id, instance_id, dimension));
+  // EA.run(dir, "EA", pbo_EA, budget, budget, runs, seed);
 
-  shared_ptr<IOHprofiler_suite<int> > pbo_ea1p1(
-      new PBO_suite(problem_id, instance_id, dimension));
-  run_ea1p1(dir, pbo_ea1p1, budget, runs, seed);
+  // shared_ptr<IOHprofiler_suite<int> > pbo_ea1p1(
+  //     new PBO_suite(problem_id, instance_id, dimension));
+  // run_ea1p1(dir, pbo_ea1p1, budget, runs, seed);
 
-  shared_ptr<IOHprofiler_suite<int> > pbo_fea1p1(
-      new PBO_suite(problem_id, instance_id, dimension));
-  run_fea1p1(dir, pbo_fea1p1, budget, runs, seed);
+  // shared_ptr<IOHprofiler_suite<int> > pbo_fea1p1(
+  //     new PBO_suite(problem_id, instance_id, dimension));
+  // run_fea1p1(dir, pbo_fea1p1, budget, runs, seed);
 
-  shared_ptr<IOHprofiler_suite<int>> pbo_sa(
-      new PBO_suite(problem_id, instance_id, dimension));
-  run_simulated_annealing_exp(dir, pbo_sa, budget, runs, seed);
+  // shared_ptr<IOHprofiler_suite<int>> pbo_sa(
+  //     new PBO_suite(problem_id, instance_id, dimension));
+  // run_simulated_annealing_exp(dir, pbo_sa, budget, runs, seed);
 
-  shared_ptr<IOHprofiler_suite<int>> pbo_sars(
-      new PBO_suite(problem_id, instance_id, dimension));
-  run_simulated_annealing_exp_rs(dir, pbo_sars, budget, runs, seed);
+  // shared_ptr<IOHprofiler_suite<int>> pbo_sars(
+  //     new PBO_suite(problem_id, instance_id, dimension));
+  // run_simulated_annealing_exp_rs(dir, pbo_sars, budget, runs, seed);
 }
